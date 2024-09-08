01a59fde5477a7e2fd1a7e8927d76ba74e5b045d
Timothy Sanders <timsanders07@gmail.com>
2024-09-08 13:08:46 -0400

    add more styling and nav functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
 // select header
+const header = document.querySelector('.header');
 // select nav
+const nav = document.querySelector('.nav');
 // select nav trigger
-
+const navTrigger = document.querySelector('.header__nav-trigger');
 // open - close menu
+navTrigger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+})
 
 // header background on scroll
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 40) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
+    }
+})
 
 // Initialize Swiper
 
